698de00735d70c484b4768098dfd71c8f67b6393
laura bosiljevac <laura.bosiljevac@gmail.com>
2018-07-09 19:55:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+javascriptfunction shout('string')
+{
+  return
+  string
+}
+'hello!'.toUpperCase()// 'HELLO!'
+function shout (string)
+{
+  return
+  string.toUpperCase() 
+  
+}
+  javascript
+expect(shout('hello')).toEqual('HELLO')
--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-
+``` javascript
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
